0ed9543bfdc5c4acaad551e6290519a2e0dc095b
hyunjunian <hyunjunian@gmail.com>
2020-11-02 13:07:29 +0900

    - first commit

--- pages/api/hello-cache.js
+++ pages/api/hello-cache.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+    res.setHeader('Cache-Control', 's-maxage=1,stale-while-revalidate');
     res.statusCode = 200
     res.json({name: 'John Doe'})
 }
